afd69c13e71d14febd3b747b4603b229225bb40b
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2017-11-28 12:41:09 -0500

    Persondriver

---
+++ Persondriver.java
@@ -0,0 +1,63 @@
+c class Persondriver {
+
+ 	public static void main (String [] args) {
+
+ 
+
+ 	Person person1 = new Person();
+
+ 	System.out.println(person1);
+
+ 	person person2 = new person("Noel", 23);
+
+ 	System.out.println(person2);
+
+
+ 	System.out.println("true");
+
+ 	} else if {
+		System.out.println("false");
+
+ 		}
+
+ 	String name2 = person2.getName();
+
+ 	if (name1.equals(name2)) {
+
+ 	System.out.println("true");
+
+
+
+ 	} else if { System.out.println("false");
+		}
+
+ 	int age1 = person1.getAge();
+
+ 	int age2 = person2.getAge();
+
+
+
+ 	if (age1==age2) {
+
+ 	System.out.println("true");
+
+ 	} else if { System.out.println("false");
+		}
+
+ 	
+
+ 	if (age1>age2) {
+
+ 		System.out.println("Rachel is older than Noel");
+
+ 		}
+
+ 	if (age2<age1){
+
+ 		System.out.println("Noel is younger than Rachel");
+
+ 	}
+
+ 
+
+ 	}
